6fef610268c44baaad43356cdf860fd5f9761c41
usernamedrew <59914219+ssvjugg@users.noreply.github.com>
2025-05-22 21:55:12 +0300

    Initial commit

---
+++ src/main/java/ru/usernamedrew/reuniteme/ReuniteMeApplication.java
@@ -0,0 +1,13 @@
+package ru.usernamedrew.reuniteme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReuniteMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReuniteMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/usernamedrew/reuniteme/ReuniteMeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.usernamedrew.reuniteme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReuniteMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
